a60b4a01eaab75bab7dc622becc7a671cc8c9ef5
nereamo <nmo.olivares@gmail.com>
2026-02-24 21:11:38 +0100

    Comentarios JavaDoc añadidos en FolderItem

--- src/main/java/montoya/mediabox/fileInformation/FolderItem.java
+++ src/main/java/montoya/mediabox/fileInformation/FolderItem.java
@@ -3,34 +3,47 @@ package montoya.mediabox.fileInformation;
 import java.io.File;
 
 /**
- * Muestra el nombre de directorio en JList pero sin mostrar la ruta completa
+ * Permite diferenciar entre los directorios de red, locales y misxtos.
+ * Muestra solo el nombre no la ruta completa.
  * 
  * @author Nerea
  */
 public class FolderItem {
 
+    /** Variables generales */
     private String fullPath;
     private boolean isNetwork;
     private boolean isBoth;
 
+    /**
+     * Constructor que inicializa los distintos directorios
+     * 
+     * @param fullPath Ruta completa del directorio
+     * @param isNetwork True si es el directorio de red
+     * @param isBoth True si es el directorio mixto
+     */
     public FolderItem(String fullPath, boolean isNetwork, boolean isBoth) {
         this.fullPath = fullPath;
         this.isNetwork = isNetwork;
         this.isBoth = isBoth;
     }
 
+    /** @return Ruta completa */
     public String getFullPath() {
         return fullPath;
     }
 
+    /** @return True si es directorio de red */
     public boolean isIsNetwork() {
         return isNetwork;
     }
 
+    /** @return True si es directorio mixto */
     public boolean isIsBoth() {
         return isBoth;
     }
 
+    /** @return Nombre del directorio */
     @Override
     public String toString() {
         return new File(fullPath).getName();
